b42e3995b94bbdc00e84e77b80e23cf77cecebd4
rikuto-hara <harariku317@gmail.com>
2025-05-30 22:48:04 +0900

    歩き、ジャンプアニメーションの追加

--- Assets/Characters/Remy/move/RemyController.cs
+++ Assets/Characters/Remy/move/RemyController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RemyController : MonoBehaviour
 {
@@ -6,14 +8,17 @@ public class RemyController : MonoBehaviour
     public float jumpForce = 5f; // Force applied when jumping
     public float Raylength = 1.1f; // Cooldown time between jumps
 
+    bool isGrounded;
+    bool wasGrounded;
+
     Rigidbody rb; // Reference to the Rigidbody component
-    Animation anim; // Reference to the Animation component (not used in this script but can be useful for animations)
+    Animator anim; // Reference to the Animation component (not used in this script but can be useful for animations)
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to this GameObject
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,13 +31,22 @@ public class RemyController : MonoBehaviour
         bool isWalking = vec.magnitude > 0.1f; // Check if the character is moving
 
         /*地面判定*/
-        bool grounded = Physics.Raycast(transform.position, Vector3.down, Raylength); // Check if the character is grounded
+        wasGrounded = isGrounded;
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, Raylength); // Check if the character is grounded
 
         /*移動*/
         Vector3 velocity = rb.linearVelocity;
         velocity.x = vec.x * speed;
         velocity.z = vec.z * speed;
         rb.linearVelocity = velocity;
+        if(Mathf.Abs(velocity.x) > 0 || Mathf.Abs(velocity.z) >0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
 
 
         /*回転の処理*/
@@ -44,28 +58,14 @@ public class RemyController : MonoBehaviour
 
         /*ジャンプの処理*/
         bool jump = Input.GetButtonDown("Jump"); // Check if the jump button is pressed
-        if (jump && grounded)
+        if (jump && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Apply an upward force to the Rigidbody for jumping
+            anim.SetBool("isJumping", true);
         }
-
-        /*アニメーションの処理*/
-        if (anim != null)
+        if (!wasGrounded && isGrounded)
         {
-            string currentAnim = anim.isPlaying ? anim.clip.name : "";
-
-            if (!grounded && currentAnim != "Jump")
-            {
-                anim.Play("Jump"); // Jumping
-            }
-            else if (isWalking && grounded && currentAnim != "Walk")
-            {
-                anim.Play("Walk"); // Walking
-            }
-            else if (!isWalking && grounded && currentAnim != "Idle")
-            {
-                anim.Play("Idle"); // Idle
-            }
+            anim.SetBool("isJumping", false);
         }
     }
 }
